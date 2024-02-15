47cffcb1ea600756b21ff67445988e58620aa755
Kolea PLESCO <kaleanych@gmail.com>
2024-02-15 22:40:48 +0200

    wip

--- public/index.php
+++ public/index.php
@@ -5,5 +5,9 @@ if (PHP_MAJOR_VERSION < 8) {
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
-echo 'Hello!';
+new \wfm\App();
+
+debug(\wfm\Router::getRoutes());
--- src/wfm/App.php
+++ src/wfm/App.php
@@ -6,12 +6,16 @@ namespace wfm;
 
 class App
 {
+
     public static Registry $app;
 
     public function __construct()
     {
+        $query = trim(urldecode($_SERVER['QUERY_STRING']), '/');
+        new ErrorHandler();
         self::$app = Registry::getInstance();
         $this->getParams();
+        Router::dispatch($query);
     }
 
     protected function getParams(): void
