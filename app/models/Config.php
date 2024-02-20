45084482733d0174f11100d5bb57924a70b7d7e9
Kolea PLESCO <kaleanych@gmail.com>
2024-02-20 07:40:24 +0200

    WIP

--- app/models/Config.php
+++ app/models/Config.php
@@ -8,9 +8,9 @@ use RedBeanPHP\R;
 class Config extends AppModel
 {
 
-    public static function getConfig($name, $lang = null): string
+    public static function getConfig($name, $lang = null): ?string
     {
         $config = R::getRow("SELECT * FROM config LIMIT 1");
-        return $config[$name];
+        return $config[$name] ?? null;
     }
 }
