0aa1d59861177cec9f688481be8d87bab5f5dbdf
Kolea PLESCO <kaleanych@gmail.com>
2024-02-20 07:45:09 +0200

    WIP

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 
 
 use app\models\AppModel;
+use app\models\Config;
 use app\models\Wishlist;
 use app\widgets\language\Language;
 use RedBeanPHP\R;
@@ -21,6 +22,7 @@ class AppController extends Controller
 
         App::$app->setProperty('languages', Language::getLanguages());
         App::$app->setProperty('language', Language::getLanguage(App::$app->getProperty('languages')));
+        App::$app->setProperty('config', Config::getAppConfig());
 
         $lang = App::$app->getProperty('language');
         \wfm\Language::load($lang['code'], $this->route);
--- app/models/Config.php
+++ app/models/Config.php
@@ -4,13 +4,19 @@ namespace app\models;
 
 use app\models\AppModel;
 use RedBeanPHP\R;
+use wfm\App;
 
 class Config extends AppModel
 {
 
+    public static function getAppConfig(): array
+    {
+        return R::getRow("SELECT * FROM config LIMIT 1");
+    }
+
     public static function getConfig($name, $lang = null): ?string
     {
-        $config = R::getRow("SELECT * FROM config LIMIT 1");
+        $config = App::$app->getProperty('config');
         return $config[$name] ?? null;
     }
 }
